e6d9d39bccdfa7661df30888929ebe237f31c78e
Thomas Dumont <acolyteofwar@gmail.com>
2017-09-14 02:24:39 +0000

    Added all files to hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+/*Prints the string in puts to the screen*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int integertype;
+	float floattype;
+	char chartype;
+	long int longinttype;
+	long long int longlonginttype;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(chartype));
+	printf("Size of int: %ld byte(s)\n", sizeof(integertype));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longinttype));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlonginttype));
+	printf("Size of a float: %ld byte(s)\n", sizeof(floattype));
+	return (0);
+}
