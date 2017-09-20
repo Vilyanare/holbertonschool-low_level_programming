5b7bb8e44bce39e66dac5ced96be21b4a1a30eda
Thomas Dumont <acolyteofwar@gmail.com>
2017-09-20 05:11:06 +0000

    Added 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ *main - prints the sum of all numbers that are divisible by 5 and 3
+ *under 1024
+ *Return: always 0
+ */
+int main(void)
+{
+	int x, count, five, three;
+
+	count = 0;
+	x = 0;
+
+	while (count < 1024)
+	{
+		count = 5 * x;
+		five += count;
+		x++;
+	}
+	x = 0;
+	while (count < 1024)
+	{
+		count = 3 * x;
+		three += count;
+		x++;
+	}
+	printf("%d\n", three + five);
+	return (0);
+}
