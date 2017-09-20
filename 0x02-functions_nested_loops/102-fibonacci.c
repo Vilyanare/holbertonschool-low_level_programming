da50049f1a1bc5765144f6749b3ed9b776fbd8d8
Thomas Dumont <acolyteofwar@gmail.com>
2017-09-20 06:49:07 +0000

    Added 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ *main - print out the first 50 numbers of the fibonacci sequence
+ *Return: always 0
+ */
+int main(void)
+{
+	long int a, b, c, count;
+
+	a = 1;
+	b = 2;
+	c = 0;
+	for (count = 0; count <= 50; count ++)
+	{
+		printf("%ld", a);
+		c = a;
+		a = b;
+		b = a + c;
+		if (count != 50)
+			printf(", ");
+	}
+	putchar('\n');
+	return (0);
+}
