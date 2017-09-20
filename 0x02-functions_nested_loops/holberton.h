6e196577266ad8ff1b9325630f5796cce8093312
Thomas Dumont <acolyteofwar@gmail.com>
2017-09-20 03:52:00 +0000

    updated holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -13,4 +13,7 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int);
 void commaspace(int);
+void print_times_table(int);
+void put_space(int);
+void put_digit(int, int);
 #endif /* HOLBERTON_H */
