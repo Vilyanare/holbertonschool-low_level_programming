3f5ff9f617c5a60d6fe1c9f7960599800212d71f
Thomas Dumont <acolyteofwar@gmail.com>
2017-09-20 19:25:00 +0000

    Changed format output of 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,15 +7,17 @@ int main(void)
 {
 	int count;
 	unsigned long int first = 0, first2 = 0, first3 = 0;
-	unsigned long int second = 1, second2 = 2, second3 = 3;
+	unsigned long int second = 1, second2 = 2, second3 = second + second2;
 	unsigned long int secondhold = 0;
 
 	for (count = 0; count <= 97; count++)
 	{
 		if (first > 0)
-			printf("%lu%lu\n", first, second);
+			printf("%lu%lu", first, second);
 		else
-			printf("%lu\n", second);
+			printf("%lu", second);
+		if (count != 97)
+			printf(", ");
 
 		first = first2;
 		first2 = first3;
@@ -25,5 +27,6 @@ int main(void)
 		second3 = secondhold % 1000000000000000;
 		first3 = ((first + first2) + (secondhold / 1000000000000000));
 	}
+	putchar('\n');
 	return (0);
 }
