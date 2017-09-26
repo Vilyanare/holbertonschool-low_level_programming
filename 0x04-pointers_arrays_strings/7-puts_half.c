7f9669160ce6580b99e1edb765fe4b9d148410c4
Thomas Dumont <acolyteofwar@gmail.com>
2017-09-26 20:54:47 +0000

    fixed typo and added newline

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -9,11 +9,12 @@ void puts_half(char *str)
 	int x, half, realsize;
 
 	realsize = sizeof(str) + 2;
-	half = realsiez / 2;
+	half = realsize / 2;
 	if (!(realsize % 2))
 		half = (realsize - 1) / 2;
 	x = realsize - half;
 
 	for ( ; x <= realsize; x++)
 		_putchar(str[x - 1]);
+	_putchar('\n');
 }
