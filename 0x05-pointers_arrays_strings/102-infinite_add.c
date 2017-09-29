02aeaab1fef2857aeec98f921f5d5359f09e6b69
Vilyanare <acolyteofwar@gmail.com>
2017-09-29 22:43:42 +0000

    102 now bety compliant and working properly

--- 0x05-pointers_arrays_strings/102-infinite_add.c
+++ 0x05-pointers_arrays_strings/102-infinite_add.c
@@ -1,25 +1,38 @@
 #include"holberton.h"
 /**
-  *rev_string - reverses input string stores in
-  *same pointer
-  *@s: input string to be reversed
-  *Return: void
+ *rev_string - reverses input string stores in
+ *same pointer
+ *@s: input string to be reversed
+ *Return: void
  */
 void rev_string(char *s)
 {
-        char temp = 0;
-        int x, c;
+	char temp = 0;
+	int x, c;
 
-        for (x = 0; s[x]; x++)
+	for (x = 0; s[x]; x++)
 		;
-        x--;
-        for (c = 0; s[c] < s[x]; c++)
-        {
+	x--;
+	for (c = 0; c < x; c++)
+	{
 		temp = s[c];
 		s[c] = s[x];
 		s[x] = temp;
 		x--;
-        }
+	}
+}
+/**
+ *str_count - count size of a string
+ *@s: string to be counted
+ *Return: string size
+ */
+int str_count(char *s)
+{
+	int x;
+
+	for (x = 0; s[x]; x++)
+		;
+	return (x);
 }
 /**
  *infinite_add - adds two numbers
@@ -31,42 +44,41 @@ void rev_string(char *s)
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-        int n1c, rc, n2c, x, carry, result;
+	int n1c, n2c, x, carry, result;
 
 	carry = 0;
-        for (n1c = 0; n1[n1c]; n1c++)
-                ;
-        for (n2c = 0; n2[n2c]; n2c++)
-                ;
+	n1c = str_count(n1);
+	n2c = str_count(n2);
 	if (n1c >= size_r)
-                return(0);
-        for (x = 0; x < size_r; x++)
-        {
-		if (n1c > 0 && n2c > 0)
-		{
-			n1c--;
-			n2c--;
+		return (0);
+	n1c--;
+	n2c--;
+	for (x = 0; x < size_r; x++)
+	{
+		if (n1c >= 0 && n2c >= 0)
 			result = (n1[n1c] - 48) + (n2[n2c] - 48) + carry;
-		}
 		else if (n1c > n2c)
-		{
-			n1c--;
 			result = (n1[n1c] - 48) + carry;
-		}
 		else if (n2c > n1c)
-		{
-			n2c--;
 			result = (n2[n2c] - 48) + carry;
-		}
 		else
 			break;
+		if ((n1c >= 0 && n2c >= 0) || n1c > n2c)
+			n1c--;
+		if ((n1c >= 0 && n2c >= 0) || n1c < n2c)
+			n2c--;
 		r[x] = result % 10 + 48;
 		carry = result / 10;
 		if (carry && x + 2 == size_r)
 			return (0);
-        }
-        if (carry)
+	}
+	if (carry)
+	{
 		r[x] = carry + 48;
-        rev_string(r);
+		r[x + 1] = '\0';
+	}
+	else
+		r[x] = '\0';
+	rev_string(r);
 	return (r);
 }
