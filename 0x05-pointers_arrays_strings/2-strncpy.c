261f75942ca5509e9fda1c02a9163a0054209855
Vilyanare <acolyteofwar@gmail.com>
2017-09-29 00:06:50 +0000

    removed unused variables

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 #include"holberton.h"
 /**
- **_strncpy - copies a string
+ *_strncpy - copies a string
  *@dest: location to copy the string to
  *@src: string to be copied
  *@n: how much of the string to copy
@@ -8,7 +8,7 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int c, x;
+	int c;
 
 	for (c = 0; c < n; c++)
 		dest[c] = src[c];
