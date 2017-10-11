be262f884d4012f254edbb2449010970bba92d1c
Vilyanare <acolyteofwar@gmail.com>
2017-10-11 04:04:50 +0000

    reconstructed 5

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -1,51 +1,6 @@
 #include"holberton.h"
+#include<stdio.h>
 #include<stdlib.h>
-/**
- *str_concat - concatenates two strings
- *@s1: first string
- *@s2: second string to add to s1
- *Return: pointer to concatenated string or NULL on failure
- */
-char *str_concat(char *s1, char *s2)
-{
-	int x = 0, c = 0, i = 0, j = 0;
-	char *p = NULL;
-
-	if (s1 != NULL)
-	{
-		for (x = 0; s1[x]; x++)
-			;
-	}
-	if (s2 != NULL)
-	{
-		for (c = 0; s2[c]; c++)
-			;
-	}
-	p = malloc((c + x + 1) * sizeof(char));
-	if (p == NULL)
-	{
-		free(p);
-		return (NULL);
-	}
-	while (j < c)
-	{
-		if (i < x)
-		{
-			if (s1 != NULL)
-				p[i] = s1[i];
-			i++;
-		}
-		else
-		{
-			if (s2 != NULL)
-				p[i + j] = s2[j];
-			j++;
-		}
-	}
-	p[x + c] = '\n';
-	p[x + c + 1] = '\0';
-	return (p);
-}
 /**
  *argstostr - concatenates all arguments of my program
  *@ac: argument count
@@ -54,16 +9,31 @@ char *str_concat(char *s1, char *s2)
  */
 char *argstostr(int ac, char **av)
 {
-	int x;
+	int x, sum = 0, c;
 	char *p = NULL;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	for (x = 0; x < ac; x++)
 	{
-		p = str_concat(p, av[x]);
-		if (p == NULL)
-			return (NULL);
+		for (c = 0; av[x][c]; c++)
+			;
+		sum += c + 1;
 	}
+	p = malloc(sum * sizeof(char) + 1);
+	if (p == NULL)
+		return (NULL);
+	sum = 0;
+	for (x = 0; x < ac; x++)
+	{
+		for (c = 0; av[x][c]; c++)
+		{
+			p[sum] = av[x][c];
+			sum++;
+		}
+		p[sum] = '\n';
+		sum++;
+	}
+	p[sum] = '\0';
 	return (p);
 }
