8de0900614f4b80daa40934180a7959177e3de07
Vilyanare <acolyteofwar@gmail.com>
2017-10-11 03:03:04 +0000

    removed initialization of p

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -9,7 +9,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	int x = 0, c = 0, i = 0, j = 0;
-	char *p = NULL;
+	char *p;
 
 	if (s1 != NULL)
 	{
@@ -23,10 +23,7 @@ char *str_concat(char *s1, char *s2)
 	}
 	p = malloc((c + x + 1) * sizeof(char));
 	if (p == NULL)
-	{
-		free(p);
 		return (NULL);
-	}
 	while (j < c)
 	{
 		if (i < x)
