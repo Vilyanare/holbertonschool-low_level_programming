7cb9180f71395eca9a2d40812dd2030603f2a1a9
Vilyanare <acolyteofwar@gmail.com>
2017-10-11 00:21:04 +0000

    added condition for null

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -14,7 +14,8 @@ char *create_array(unsigned int size, char c)
 	if (size < 1)
 		return (NULL);
 	p = malloc(size * sizeof(char));
-
+	if (p = NULL)
+		return (NULL);
 	for (x = 0; x < size; x++)
 		p[x] = c;
 	p[x] = '\0';
