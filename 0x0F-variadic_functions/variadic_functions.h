be3b70ed60dd6ff915eadedf2524c1503de83bb5
Vilyanare <acolyteofwar@gmail.com>
2017-10-21 20:28:32 +0000

    Forgot to upload variadic_functions.h

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,5 +1,10 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+
+#include<stdlib.h>
+#include<stdarg.h>
+#include<stdio.h>
+
 /**
  *struct formatspecifier - format decider
  *@fmt: format character
@@ -10,6 +15,7 @@ typedef struct formatspecifier
 	char *fmt;
 	void (*f)(va_list);
 } fmt_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *sparator, const unsigned int n, ...);
