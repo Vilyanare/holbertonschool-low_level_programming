296a3e1add955870058dfaf314e9b608ed0e9939
Vilyanare <acolyteofwar@gmail.com>
2017-10-21 20:12:12 +0000

    3 now betty compliant

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -2,46 +2,37 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 /**
-  *struct formatspecifier - format decider
-  *@fmt: format character
-  *@f: function to do with said format
-  */
-typedef struct formatspecifier
-{
-	char *fmt;
-	void (*f)(va_list);
-} fmt_t;
-/**
   *printchar - print a char
-  *@s: character to print
-  */
-void printchar(va_list(prnall))
+ *@prnall: char to print
+ */
+void printchar(va_list prnall)
 {
 	printf("%c", va_arg(prnall, int));
 }
 /**
   *printint - print integer
-  *@s: number to print
-  */
-void printint(va_list(prnall))
+ *@prnall: int to print
+ */
+void printint(va_list prnall)
 {
 	printf("%d", va_arg(prnall, int));
 }
 /**
   *printfloat - print floating point number
-  *@s: number to print
-  */
-void printfloat(va_list(prnall))
+ *@prnall: float to print
+ */
+void printfloat(va_list prnall)
 {
 	printf("%f", va_arg(prnall, double));
 }
 /**
   *printstring - print (nil) if NULL or the string
-  *@s: string to print
-  */
-void printstring(va_list(prnall))
+ *@prnall: string to print
+ */
+void printstring(va_list prnall)
 {
 	char *s = va_arg(prnall, char*);
+
 	if (s == NULL)
 	{
 		printf("(nil)");
@@ -58,13 +49,14 @@ void print_all(const char * const format, ...)
 	va_list prnall;
 	int x = 0, c = 0;
 	char *sep = "\0";
-	fmt_t fmt[]= {
+	fmt_t fmt[] = {
 		{"c", printchar},
 		{"i", printint},
 		{"f", printfloat},
 		{"s", printstring},
 		{NULL, NULL},
 	};
+
 	va_start(prnall, NULL);
 	while (format[x])
 	{
--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,6 +1,15 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
-
+/**
+ *struct formatspecifier - format decider
+ *@fmt: format character
+ *@f: function to do with said format
+ */
+typedef struct formatspecifier
+{
+	char *fmt;
+	void (*f)(va_list);
+} fmt_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *sparator, const unsigned int n, ...);
