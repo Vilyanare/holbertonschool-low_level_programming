033d1c9e447804f919cca3cc9a109c863ad356e9
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2017-11-07 21:38:42 -0800

    Changed 3

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,87 +1,66 @@
 #include "holberton.h"
 /**
- * txt_buf - reads a text file and returns buffer
- * @filename: name of text file
- * @letters: how many characters to read
- * Return: address of buffer
+ * error_func - take in a number and print an error
+ * @error: contains error number
  */
-char *txt_buf(const char *filename, size_t letters)
+void error_func(int error, ...)
 {
-	int fd = 0;
+	va_list a;
+	va_start(a, NULL);
+	if (error == 97)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	else if (error == 98)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", va_arg(a, char *));
+		exit(98);
+	}
+	else if (error == 99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", va_arg(a, char *));
+		exit(99);
+	}
+	else if (error == 100)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", va_arg(a, int));
+		exit(100);
+	}
+	va_end(a);
+}
+/**
+ * txt_buf - reads from file1 and writes to file2
+ * @filename: name of text file
+ * @file2: file to write to
+ */
+void txt_buf(char *filename, char *file2)
+{
+	int fd1 = 0, fd2 = 0;
 	int countr = 0;
 	char *buf = NULL;
 
-	buf = malloc(sizeof(char) * letters);
+	buf = malloc(sizeof(char) * BUF_SIZE);
 	if (buf == NULL)
-		return (NULL);
-	fd = open(filename, O_RDONLY);
-	if (fd < 0 || filename == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
-	}
-	countr = read(fd, buf, letters);
-	if (countr < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
-	}
-	if (close(fd) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-	if (countr == 1024)
-	{
-		buf[1023] = '\0';
-		return (buf);
-	}
-	else
-	{
-		buf[countr + 2] = '\0';
-		return (buf);
-	}
-}
-/**
- * append_text_to_file - appends text to a file
- * @filename: name of file to append
- * @text_content: text to append to file
- * Return: 1 on success -1 on failure
- */
-int append_text_to_file(const char *filename, char *text_content)
-{
-	int fd = 0, x = 0, t = 0;
+		return;
+	fd1 = open(filename, O_RDONLY);
+	if (fd1 < 0)
+		error_func(98, filename);
+	fd2 = open(file2, O_WRONLY | O_APPEND);
+	if (fd2 < 0)
+		error_func(99, file2);
+	do {
+		countr = read(fd1, buf, BUF_SIZE);
+		if (countr < 0)
+			error_func(98);
+		if (write (fd2, buf, countr) < 0)
+			error_func(99);
+	} while (countr == BUF_SIZE);
+	if (close(fd1) < 0)
+		error_func(100, fd1);
+	if (close(fd2) < 0)
+		error_func(100, fd2);
 
-	if (filename == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
-	if (text_content != NULL)
-	{
-		for (; text_content[x]; x++)
-			;
-	}
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
-	if (x > 0)
-		if (write(fd, text_content, x) < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-			exit(99);
-		}
-	if (close(fd) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-	if (t < 0)
-		return (-1);
-	return (1);
 }
 /**
  * creat_fil - creates a file
@@ -93,21 +72,12 @@ int creat_fil(char *filename)
 	int fd = 0;
 
 	if (filename == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
+		error_func(99, filename);
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
+		error_func(99, filename);
 	if (close(fd) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
+		error_func(100, fd);
 	return (1);
 }
 /**
@@ -122,30 +92,10 @@ int creat_fil(char *filename)
  */
 int main(int argc, char **argv)
 {
-	int x = 0;
-	char *temp = NULL;
-
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		error_func(97);
 	creat_fil(argv[2]);
-	temp = txt_buf(argv[1], BUF_SIZE);
-	append_text_to_file(argv[2], temp);
-	for (x = 0; temp[x]; x++)
-		;
-	free(temp);
-	if (x + 1 == BUF_SIZE)
-	{
-		do {
-			temp = txt_buf(argv[1], BUF_SIZE);
-			append_text_to_file(argv[2], temp);
-			for (x = 0; temp[x]; x++)
-				;
-			free(temp);
-		} while (x + 1 == BUF_SIZE);
-	}
+	txt_buf(argv[1], argv[2]);
 	return (0);
 }
 
