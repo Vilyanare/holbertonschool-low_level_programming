8e6620d57e520d59bb4595f07a8c4ad5cc8fbbc5
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2018-03-07 20:15:04 -0800

    Now uses size and full check instead of height check

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -1,39 +1,35 @@
 #include "binary_trees.h"
 /**
- * rec_height - recursively finds the height of a binary tree
- * @tree: binary tree to find height of
- * Return: height of the tree
+ * rec_size - recursively finds the size of a binary tree
+ * @tree: binary tree to find size of
+ * Return: size of the tree
  */
-size_t rec_height(const binary_tree_t *tree)
+size_t rec_size(const binary_tree_t *tree)
 {
 	size_t left = 0, right = 0;
 
+	if (!tree)
+		return (0);
 	if (tree->left)
-		left = rec_height(tree->left) + 1;
+		left = rec_size(tree->left) + 1;
 	if (tree->right)
-		right = rec_height(tree->right) + 1;
-	return ((left > right) ? left : right);
+		right = rec_size(tree->right) + 1;
+	return (left + right);
 }
 /**
- * rec_full_height - check that all nodes children have equal height
- * @node: node to check if equal
- * Return: 1 if all equal otherwise 0
+ * rec_full - check to see if a binary tree is full
+ * @tree: tree to check if full
+ * Return: 1 if full 0 otherwise
  */
-int rec_full_height(const binary_tree_t *node)
+int rec_full(const binary_tree_t *tree)
 {
-	int left = 0, right = 0, lt = 1, rt = 1;
+	int left = 0, right = 0;
 
-	if (node->left)
-	{
-		left = rec_height(node->left);
-		lt = rec_full_height(node->left);
-	}
-	if (node->right)
-	{
-		right = rec_height(node->right);
-		rt = rec_full_height(node->right);
-	}
-	return (((right == left) ? 1 : 0) & rt & lt);
+	if (tree->left)
+		left = rec_full(tree->left);
+	if (tree->right)
+		right = rec_full(tree->right);
+	return ((left == right) ? 1 : 0);
 }
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
@@ -44,6 +40,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return (rec_full_height(tree));
-
+	return (rec_full(tree) & (rec_size(tree->left) == rec_size(tree->right)));
 }
