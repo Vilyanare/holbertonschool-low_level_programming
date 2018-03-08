8a2c15b1e94a9efcb537f8069eaa671519b00c2e
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2018-03-07 19:42:17 -0800

    Uses function to check and make sure  every branch is the same height.  removed full tree check

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -15,19 +15,25 @@ size_t rec_height(const binary_tree_t *tree)
 	return ((left > right) ? left : right);
 }
 /**
- * rec_full - check to see if a binary tree is full
- * @tree: tree to check if full
- * Return: 1 if full 0 otherwise
+ * rec_full_height - check that all nodes children have equal height
+ * @node: node to check if equal
+ * Return: 1 if all equal otherwise 0
  */
-int rec_full(const binary_tree_t *tree)
+int rec_full_height(const binary_tree_t *node)
 {
 	int left = 0, right = 0;
 
-	if (tree->left)
-		left = rec_full(tree->left);
-	if (tree->right)
-		right = rec_full(tree->right);
-	return ((left == right) ? 1 : 0);
+	if (node->left)
+	{
+		left = rec_height(node->left);
+		rec_full_height(node->left);
+	}
+	if (node->right)
+	{
+		right = rec_height(node->right);
+		rec_full_height(node->right);
+	}
+	return ((right == left) ? 1 : 0);
 }
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
@@ -38,5 +44,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return ((rec_height(tree->left) == rec_height(tree->right)) & rec_full(tree));
+	return (rec_full_height(tree));
+
 }
